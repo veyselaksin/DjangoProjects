4b20f33eb58c54f8536472329bbafde5cc13ba5f
Veysel Aksin <32255510+veyselaksin@users.noreply.github.com>
2022-07-29 23:06:24 +0300

    Base app created (#3)
    * base-app created & setup project
    
    * portfolio app created & done.

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/dev/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'pages.apps.PagesConfig',
+    'base.apps.BaseConfig'
 ]
 
 MIDDLEWARE = [
@@ -116,6 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'img/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/dev/ref/settings/#default-auto-field
