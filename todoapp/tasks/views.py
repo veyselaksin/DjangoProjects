fd5bc697cb4bd86aa305a8a0b107e6e66cdc3ca4
Veysel Aksin <32255510+veyselaksin@users.noreply.github.com>
2022-07-31 12:21:00 +0300

    To do app (#6)
    * TODO App Structure Created!
    
    * Tasks & Pages app created!
    
    * Todo App has done!

--- todoapp/tasks/views.py
+++ todoapp/tasks/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .models import Task
+from .froms import TaskForm
 
 # Create your views here.
+def tasks(request): 
+    all_task = Task.objects.all()
+
+    task_form = TaskForm()
+
+    if request.method == 'POST':
+        task_form = TaskForm(request.POST)
+
+        if task_form.is_valid():
+            task_form.save()
+        return redirect("/tasks")
+
+    context = {
+        'tasks': all_task,
+        'task_form': task_form
+    }
+    return render(request, 'tasks/tasks.html', context)
+
+def update_task(request, pk):
+    task = Task.objects.get(id=pk)
+
+    task_form = TaskForm(instance=task)
+
+    if request.method == "POST":
+        task_form = TaskForm(request.POST, instance=task)
+
+        if task_form.is_valid():
+            task_form.save()
+        return redirect("/tasks")
+    
+    context = {
+        "task_form": task_form
+    }
+
+    return render(request, "tasks/update_task.html", context)
+
+def delete_task(request, pk):
+    task = Task.objects.get(id=pk)
+
+    if request.method == "POST":
+        task.delete()
+        return redirect("/tasks")
+    
+    context = {
+        "task": task
+    }
+    return render(request, "tasks/delete_task.html", context)
--- todoapp/todoapp/settings.py
+++ todoapp/todoapp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/dev/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'pages.apps.PagesConfig',
     'tasks.apps.TasksConfig'
 ]
 
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'todoapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,6 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/dev/ref/settings/#default-auto-field
