975270289bba7712283247c555e4f0dd33537499
Veysel AKSIN <veyselaksn@gmail.com>
2022-08-04 23:26:39 +0300

    generated new views

--- alwazah/tasks/urls.py
+++ alwazah/tasks/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.TaskList.as_view(), name="task_list")
+    path('', views.TaskList.as_view(), name="task_list"),
+    path('task/<int:pk>', views.TaskDetail.as_view(), name="task_detail"),
+    path('task_create/', views.TaskCreate.as_view(), name="task_create"),
+    path('task_update/<int:pk>/', views.TaskUpdate.as_view(), name="task_update")
 ]
--- alwazah/tasks/views.py
+++ alwazah/tasks/views.py
@@ -1,5 +1,26 @@
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView
 from .models import Task
+from typing import Optional
+from django.urls import reverse_lazy
+
 # Create your views here.
 class TaskList(ListView):
     model = Task
+    context_object_name = 'tasks'
+    template_name: str = 'tasks/tasks.html'
+
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name: Optional[str] = 'task'
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('task_list')
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('task_list')
