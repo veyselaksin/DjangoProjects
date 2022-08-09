4b2102238315ccf0dc39f6596851ff537683c7c6
Veysel Aksin <32255510+veyselaksin@users.noreply.github.com>
2022-08-09 23:21:04 +0300

    Revert "Kwin (#11)"
    This reverts commit 3e47c9551b10fc64a3d414697be2802af36328fd.

--- kwin/pages/urls.py
+++
@@ -1,9 +0,0 @@
-from django import views
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name="home"),
-    path('products', views.products, name="products"),
-    path('customers', views.customers, name="customers")
-]
