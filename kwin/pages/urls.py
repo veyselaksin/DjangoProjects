1edc6561fa484f7b934a1acee2785c27af58e509
Veysel AKSIN <veyselaksn@gmail.com>
2022-08-09 23:20:29 +0300

    Product & Customer Page Designed

--- kwin/pages/urls.py
+++ kwin/pages/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('products', views.products, name="products"),
+    path('customers', views.customers, name="customers")
 ]
--- kwin/pages/views.py
+++ kwin/pages/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from . import helper
 
 # Create your views here.
 def home(request):
     return render(request, 'pages/home.html')
+
+def products(request):
+    return render(request, 'pages/products.html')
+
+def customers(request):
+    return render(request, 'pages/customers.html')
